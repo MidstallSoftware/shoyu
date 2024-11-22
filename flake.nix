ca1002096e49db179b45f18ff7739c45f9fe0b90
Tristan Ross <tristan.ross@midstall.com>
2024-11-22 13:44:45 -0800

    feat: improve flake

--- flake.nix
+++ flake.nix
@@ -22,89 +22,105 @@
     }:
     let
       inherit (nixpkgs) lib;
+
+      defaultOverlay =
+        pkgs: prev: with pkgs; {
+          shoyu = stdenv.mkDerivation {
+            pname = "shoyu";
+            version = self.shortRev or "dirty";
+
+            outputs = [
+              "out"
+              "dev"
+              "devdoc"
+            ];
+
+            src = lib.cleanSource self;
+
+            nativeBuildInputs = with pkgs; [
+              ninja
+              meson
+              python3
+              gobject-introspection
+              gi-docgen
+              pkg-config
+              vala
+              wayland-scanner
+            ];
+
+            buildInputs = [
+              glib
+              wlroots_0_18
+              gtk3
+              gtk4
+              libxkbcommon
+            ];
+
+            mesonFlags = [
+              (lib.mesonBool "documentation" true)
+            ];
+
+            postPatch = ''
+              files=(
+                build-aux/meson/gen-visibility-macros.py
+              )
+
+              chmod +x ''${files[@]}
+              patchShebangs ''${files[@]}
+            '';
+
+            postFixup = ''
+              moveToOutput "share/doc" "$devdoc"
+            '';
+          };
+        };
     in
     flake-utils.lib.eachSystem (import systems) (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system}.extend (
-          pkgs: prev: with pkgs; {
-            pkgsAsahi = (
-              if stdenv.hostPlatform.isAarch64 then
-                pkgs.appendOverlays [
-                  nixos-apple-silicon.overlays.default
-                  (pkgsAsahi: prev: {
-                    mesa = pkgsAsahi.mesa-asahi-edge;
-                  })
-                ]
-              else
-                null
-            );
-
-            shoyu = stdenv.mkDerivation {
-              pname = "shoyu";
-              version = self.shortRev or "dirty";
-
-              outputs = [ "out" "dev" "devdoc" ];
-
-              src = lib.cleanSource self;
-
-              nativeBuildInputs = with pkgs; [
-                ninja
-                meson
-                python3
-                gobject-introspection
-                gi-docgen
-                pkg-config
-                vala
-                wayland-scanner
-              ];
-
-              buildInputs = [
-                glib
-                wlroots_0_18
-                gtk3
-                gtk4
-                libxkbcommon
-              ];
-
-              mesonFlags = [
-                (lib.mesonBool "documentation" true)
-              ];
-
-              postPatch = ''
-                files=(
-                  build-aux/meson/gen-visibility-macros.py
-                )
-
-                chmod +x ''${files[@]}
-                patchShebangs ''${files[@]}
-              '';
-
-              postFixup = ''
-                moveToOutput "share/doc" "$devdoc"
-              '';
-            };
-          }
-        );
+        pkgs = nixpkgs.legacyPackages.${system}.appendOverlays [
+          (
+            pkgs: prev: with pkgs; {
+              pkgsAsahi = (
+                if stdenv.hostPlatform.isAarch64 then
+                  pkgs.appendOverlays [
+                    nixos-apple-silicon.overlays.default
+                    (pkgsAsahi: prev: {
+                      mesa = pkgsAsahi.mesa-asahi-edge;
+                    })
+                  ]
+                else
+                  null
+              );
+            }
+          )
+          defaultOverlay
+        ];
       in
       {
-        packages = {
-          default = pkgs.shoyu;
-          llvm = pkgs.pkgsLLVM.shoyu;
-        }
-        // lib.optionalAttrs (pkgs.pkgsAsahi != null) {
-          asahi = pkgs.pkgsAsahi.shoyu;
-          asahi-llvm = pkgs.pkgsLLVM.pkgsAsahi.shoyu;
-        };
-
-        devShells = {
+        packages =
+          {
             default = pkgs.shoyu;
-            llvm = pkgs.pkgsLLVM.shoyu;
           }
           // lib.optionalAttrs (pkgs.pkgsAsahi != null) {
             asahi = pkgs.pkgsAsahi.shoyu;
-            asahi-llvm = pkgs.pkgsLLVM.pkgsAsahi.shoyu;
           };
+
+        devShells =
+          {
+            default = pkgs.shoyu;
+          }
+          // lib.optionalAttrs (pkgs.pkgsAsahi != null) {
+            asahi = pkgs.pkgsAsahi.shoyu;
+          };
+
+        legacyPackages = pkgs;
       }
-    );
+    )
+    // {
+      overlays = {
+        default = defaultOverlay;
+        shoyu = defaultOverlay;
+      };
+    };
 }
